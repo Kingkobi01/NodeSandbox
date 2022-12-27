39ab9f8bf416488fe96ecd2e5695bce45b5cb3d9
Kwabena Boakye <kwaboakye93@gmail.com>
2022-12-27 11:14:16 +0000

    Added some more files

--- 2-modules.js
+++ 2-modules.js
@@ -5,12 +5,10 @@ const secret = "SUPER SECRET"
 const john = "John"
 const peter = "Peter"
 
-const sayHi = name => console.log(`Hi, ${name}!`);
 
-sayHi('Kwabena');
-sayHi(john);
-sayHi(peter);
+// sayHi('Kwabena');
+// sayHi(john);
+// sayHi(peter);
 
 module.exports = {john, peter}
 
-console.log(module.exports);
---
+++ 4-mind-grenade.js
@@ -0,0 +1,9 @@
+const num1 = 10, num2 = 5;
+
+const addValues = () => console.log(`The sum is ${num1 + num2}`);
+
+
+addValues()
+// When you import a module that has a called function in it you have basically
+// invoked it.
+
